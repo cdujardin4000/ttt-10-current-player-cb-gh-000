c8c6e85b248e0aead0ea763921bd523af432a938
Cedric Dujardin <cedric.dujardin@laposte.net>
2019-12-17 00:45:53 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,14 @@
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  elsif board[index] == "X" || board[index] == "O"
+    return true
+  end
+end
+
 def turn_count(board)
   counter = 0
-  board.each do |counter|
+  board.each do position_taken?(board, index)
+    if true counter += 1
   end
 end
