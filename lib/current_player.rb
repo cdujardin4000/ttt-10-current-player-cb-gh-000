0f86e92cb0fdf4fbaeea0eee5b8800c36842123e
Cedric Dujardin <cedric.dujardin@laposte.net>
2019-12-17 01:20:28 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -7,9 +7,14 @@ def turn_count(board)
   board.each do board[index] = value
     if value == "X" || value == "O"
       counter += 1
+      
+      end
+    end
 end
-end
-end
-def current_player
-
+def current_player(counter)
+  if counter.even? == true
+    return "X"
+  else
+    return "O"
+  end
 end
